34d72e470bbb0723be33fbfc57af1481f710eb07
Jorge <jorgemarpa@ug.uchile.cl>
2021-06-22 16:10:02 -0700

    starting ffi unit tests

---
+++ tests/test_ffimachine.py
@@ -0,0 +1,21 @@
+"""
+This contains a collection of functions to test the Machine API
+"""
+import os
+import numpy as np
+import pandas as pd
+import pytest
+import lightkurve as lk
+from astropy.utils.data import get_pkg_data_filename
+from astropy.time import Time
+
+from psfmachine import Machine, FFIMachine
+
+from psfmachine.utils import do_tiled_query
+
+ffi = None
+
+
+@pytest.mark.remote_data
+def test_ffi_from_file():
+    raise NotImplementedError
