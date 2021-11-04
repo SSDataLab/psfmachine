6c0aaa928947a529b9c4016d3038a26a7fdd0ac1
Jorge <jorgemarpa@ug.uchile.cl>
2021-11-04 15:49:22 -0700

    new supersamp module

---
+++ src/psfmachine/superstamp.py
@@ -0,0 +1,498 @@
+"""Subclass of `Machine` that Specifically work with FFIs"""
+import os
+import numpy as np
+import lightkurve as lk
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.animation as animation
+from ipywidgets import interact
+
+from astropy.io import fits
+from astropy.time import Time
+from astropy.wcs import WCS
+import astropy.units as u
+
+from .ffi import FFIMachine
+from .ffi import _get_sources
+from .version import __version__
+
+__all__ = ["SSMachine"]
+
+
+class SSMachine(FFIMachine):
+    """
+    Subclass of Machine for working with FFI data. It is a subclass of Machine
+    """
+
+    def __init__(
+        self,
+        time,
+        flux,
+        flux_err,
+        ra,
+        dec,
+        sources,
+        column,
+        row,
+        wcs=None,
+        limit_radius=32.0,
+        n_r_knots=10,
+        n_phi_knots=15,
+        n_time_knots=10,
+        n_time_points=200,
+        time_radius=8,
+        cut_r=6,
+        rmin=1,
+        rmax=16,
+        meta=None,
+    ):
+        """
+        Class to work with FFI data.
+        Parameters
+        ----------
+        Attributes
+        ----------
+        """
+        # init `FFImachine` object
+        super().__init__(
+            time,
+            flux,
+            flux_err,
+            ra,
+            dec,
+            sources,
+            column,
+            row,
+            wcs=wcs,
+            limit_radius=limit_radius,
+            n_r_knots=n_r_knots,
+            n_phi_knots=n_phi_knots,
+            n_time_knots=n_time_knots,
+            n_time_points=n_time_points,
+            time_radius=time_radius,
+            cut_r=cut_r,
+            rmin=rmin,
+            rmax=rmax,
+            meta=meta,
+        )
+
+        self.meta["DCT_TYPE"] = "SuperStamp"
+
+    def fit_frame_models(self):
+        """
+        Fits shape model per frame (cadence)
+        """
+        prior_mu = self.source_flux_estimates  # np.zeros(A.shape[1])
+        prior_sigma = (
+            np.ones(self.mean_model.shape[0])
+            * 5
+            * np.abs(self.source_flux_estimates) ** 0.5
+        )
+        self.model_flux_frame = np.zeros(self.flux.shape) * np.nan
+        self.ws_frame = np.zeros((self.nt, self.nsources))
+        self.werrs_frame = np.zeros((self.nt, self.nsources))
+        f = self.flux
+        fe = self.flux_err
+        for tdx in tqdm(
+            range(self.nt),
+            desc=f"Fitting {self.nsources} Sources (per frame model)",
+            disable=self.quiet,
+        ):
+            X = self.mean_model_frame[tdx].copy().T
+            sigma_w_inv = X.T.dot(X.multiply(1 / fe[tdx][:, None] ** 2)).toarray()
+            sigma_w_inv += np.diag(1 / (prior_sigma ** 2))
+            B = X.T.dot((f[tdx] / fe[tdx] ** 2))
+            B += prior_mu / (prior_sigma ** 2)
+            self.ws_frame[tdx] = np.linalg.solve(sigma_w_inv, np.nan_to_num(B))
+            self.werrs_frame[tdx] = np.linalg.inv(sigma_w_inv).diagonal() ** 0.5
+            self.model_flux_frame[tdx] = X.dot(self.ws_frame[tdx])
+            nodata = np.asarray(self.source_mask.sum(axis=1))[:, 0] == 0
+            # These sources are poorly estimated
+            nodata |= (self.mean_model_frame[tdx].max(axis=1) > 1).toarray()[:, 0]
+            self.ws_frame[tdx, nodata] *= np.nan
+            self.werrs_frame[tdx, nodata] *= np.nan
+
+    def build_frames_shape_model(self, plot=False, **kwargs):
+        """
+        Compute shape model for every cadence (frame).
+        If plot is True, will generate a GIF file with PRF model evolution across
+        cadences.
+        """
+        self.mean_model_frame = []
+        images = []
+        self._get_source_mask()
+        self._get_uncontaminated_pixel_mask()
+        # check if the original source mask is the mean frame or an especific one
+        org_sm = self.source_mask
+        org_usm = self.uncontaminated_source_mask
+        fig = plt.figure(figsize=(9.5, 6.5))
+        for cno in tqdm(range(self.nt), desc=f"Building shape model per frame"):
+            img = self.build_shape_model(cadenceno=cno, plot=plot, **kwargs)
+            self.mean_model_frame.append(self.mean_model)
+            if plot:
+                # fig.canvas.draw()  # draw the canvas, cache the renderer
+                # image = np.frombuffer(fig.canvas.tostring_rgb(), dtype="uint8")
+                # image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+                # images.append(image)
+                # plt.close()
+                images.append([img])
+            self.source_mask = org_sm
+            self.uncontaminated_source_mask = org_usm
+        if plot:
+            if hasattr(self, "meta"):
+                gif_name = "./shape_models_%s_%s_c%i.mp4" % (
+                    self.meta["MISSION"],
+                    self.meta["OBJECT"],
+                    self.meta["QUARTER"],
+                )
+            else:
+                gif_name = "./shape_models_%s_q%i.mp4" % (
+                    self.tpf_meta["mission"][0],
+                    self.tpf_meta["quarter"][0],
+                )
+            # imageio.mimsave(gif_name, images, fps=24)
+            fig = plt.plot()
+            ani = animation.ArtistAnimation(
+                fig, images, interval=50, blit=True, repeat_delay=1000, repeat=True
+            )
+            plt.show()
+            # ani.save("dynamic_images.mp4")
+
+    @staticmethod
+    def from_file(fname, magnitude_limit=18, **kwargs):
+        """
+        Reads data from files and initiates a new FFIMachine class.
+        Parameters
+        ----------
+        fname : str
+            Filename of the FFI file
+        **kwargs : dictionary
+            Keyword arguments that defines shape model in a `machine` class object.
+        Returns
+        -------
+        FFIMachine : Machine object
+            A Machine class object built from the FFI.
+        """
+        (
+            wcs,
+            time,
+            flux,
+            flux_err,
+            ra,
+            dec,
+            column,
+            row,
+            metadata,
+        ) = _load_file(fname)
+
+        sources = _get_sources(
+            ra,
+            dec,
+            wcs,
+            magnitude_limit=magnitude_limit,
+            epoch=time.jyear.mean(),
+            ngrid=(2, 2) if flux.shape[1] <= 500 else (5, 5),
+            dr=3,
+            img_limits=[[row.min(), row.max()], [column.min(), column.max()]],
+        )
+
+        return SSMachine(
+            time.jd,
+            flux,
+            flux_err,
+            ra.ravel(),
+            dec.ravel(),
+            sources,
+            column.ravel(),
+            row.ravel(),
+            wcs=wcs,
+            meta=metadata,
+            **kwargs,
+        )
+
+    def fit_lightcurves(
+        self, plot=False, fit_va=True, iter_negative=True, load_shape_model=False
+    ):
+        """
+        Fit the sources inside the TPFs passed to `TPFMachine`.
+        Parameters
+        ----------
+        plot : bool
+            Whether or not to show some diagnostic plots. These can be helpful
+            for a user to see if the PRF and time dependent models are being calculated correctly.
+        fit_va : bool
+            Whether or not to fit Velocity Aberration (which implicitly will try to fit
+            other kinds of time variability). This will try to fit the "long term"
+            trends in the dataset. If True, this will take slightly longer to fit.
+            If you are interested in short term phenomena, like transits, you may
+            find you do not need this to be set to True. If you have the time, it
+            is recommended to run it.
+        iter_negative : bool
+            When fitting light curves, it isn't possible to force the flux to be positive.
+            As such, when we find there are light curves that deviate into negative
+            flux values, we can clip these targets out of the analysis and rerun the model.
+            If iter_negative is True, PSFmachine will run up to 3 times, clipping out
+            any negative targets each round.
+        """
+        # use PRF model from FFI or create one with TPF data
+        if load_shape_model:
+            self.load_shape_model(plot=plot)
+        else:
+            self.build_shape_model(plot=plot)
+        self.build_time_model(plot=plot)
+        self.fit_model(fit_va=fit_va)
+        if iter_negative:
+            # More than 2% negative cadences
+            negative_sources = (self.ws_va < 0).sum(axis=0) > (0.02 * self.nt)
+            idx = 1
+            while len(negative_sources) > 0:
+                self.mean_model[negative_sources] *= 0
+                self.fit_model(fit_va=fit_va)
+                negative_sources = np.where((self.ws_va < 0).all(axis=0))[0]
+                idx += 1
+                if idx >= 3:
+                    break
+
+        self.lcs = []
+        for idx, s in self.sources.iterrows():
+            meta = {
+                "ORIGIN": "PSFMACHINE",
+                "APERTURE": "PSF",
+                "LABEL": s.designation,
+                # "TARGETID": targetid,
+                "MISSION": self.meta["MISSION"],
+                "RA": s.ra,
+                "DEC": s.dec,
+                "PMRA": s.pmra / 1000,
+                "PMDEC": s.pmdec / 1000,
+                "PARALLAX": s.parallax,
+                "GMAG": s.phot_g_mean_mag,
+                "RPMAG": s.phot_rp_mean_mag,
+                "BPMAG": s.phot_bp_mean_mag,
+            }
+
+            attrs = [
+                "channel",
+                "module",
+                "ccd",
+                "camera",
+                "quarter",
+                "campaign",
+                "quarter",
+                "row",
+                "column",
+                "mission",
+            ]
+            for attr in attrs:
+                if attr in self.meta.keys():
+                    meta[attr.upper()] = self.meta[attr]
+
+            if fit_va:
+                flux, flux_err = (
+                    (self.ws_va[:, idx]) * u.electron / u.second,
+                    self.werrs_va[:, idx] * u.electron / u.second,
+                )
+            else:
+                flux, flux_err = (
+                    (self.ws[:, idx]) * u.electron / u.second,
+                    self.werrs[:, idx] * u.electron / u.second,
+                )
+            lc = lk.KeplerLightCurve(
+                time=(self.time) * u.d,
+                flux=flux,
+                flux_err=flux_err,
+                meta=meta,
+                time_format="jd",
+            )
+            if fit_va:
+                lc["flux_NVA"] = (self.ws[:, idx]) * u.electron / u.second
+                lc["flux_err_NVA"] = (self.werrs[:, idx]) * u.electron / u.second
+            self.lcs.append(lc)
+            self.lcs = lk.LightCurveCollection(self.lcs)
+        return
+
+    def plot_image_interactive(self, ax=None, sources=False):
+        """
+        Function to plot the Full Frame Image and the Gaia Sources
+        Parameters
+        ----------
+        ax : matplotlib.axes
+            Matlotlib axis can be provided, if not one will be created and returned
+        sources : boolean
+            Whether to overplot or not the source catalog
+        Returns
+        -------
+        ax : matplotlib.axes
+            Matlotlib axis with the figure
+        """
+        if ax is None:
+            fig, ax = plt.subplots(1, figsize=(10, 10))
+
+        ax = plt.subplot(projection=self.wcs)
+        row_2d, col_2d = np.mgrid[
+            self.row.min() : self.row.max() + 1,
+            self.column.min() : self.column.max() + 1,
+        ]
+        im = ax.pcolormesh(
+            col_2d,
+            row_2d,
+            self.flux_2d[0],
+            cmap=plt.cm.viridis,
+            shading="nearest",
+            norm=colors.SymLogNorm(linthresh=200, vmin=0, vmax=2000, base=10),
+            rasterized=True,
+        )
+        plt.colorbar(im, ax=ax, label=r"Flux ($e^{-}s^{-1}$)", fraction=0.042)
+
+        ax.set_xlabel("R.A. [hh:mm]")
+        ax.set_ylabel("Decl. [deg]")
+        ax.set_xlim(self.column.min() - 2, self.column.max() + 2)
+        ax.set_ylim(self.row.min() - 2, self.row.max() + 2)
+
+        ax.set_aspect("equal", adjustable="box")
+
+        def update(t):
+            ax.pcolormesh(
+                col_2d,
+                row_2d,
+                self.flux_2d[t],
+                cmap=plt.cm.viridis,
+                shading="nearest",
+                norm=colors.SymLogNorm(linthresh=200, vmin=0, vmax=2000, base=10),
+                rasterized=True,
+            )
+            ax.set_title(
+                "%s FFI Ch/CCD %s MJD %f"
+                % (self.meta["MISSION"], self.meta["EXTENSION"], self.time[t])
+            )
+            if sources:
+                ax.scatter(
+                    self.sources.column,
+                    self.sources.row,
+                    facecolors="none",
+                    edgecolors="r",
+                    linewidths=0.5 if self.sources.shape[0] > 1000 else 1,
+                    alpha=0.9,
+                )
+            ax.grid(True, which="major", axis="both", ls="-", color="w", alpha=0.7)
+            fig.canvas.draw_idle()
+
+        interact(update, t=(0, self.flux_2d.shape[0] - 1, 1))
+
+        return
+
+
+def _load_file(fname):
+    """
+    Helper function to load FFI files and parse data.
+    Parameters
+    ----------
+    fname : string or list of strings
+        Name of the FFI files
+    Returns
+    -------
+    wcs : astropy.wcs
+        World coordinates system solution for the FFI. Used to convert RA, Dec to pixels
+    time : numpy.array
+        Array with time values in MJD
+    flux_2d : numpy.ndarray
+        Array with 2D (image) representation of flux values
+    flux_err_2d : numpy.ndarray
+        Array with 2D (image) representation of flux errors
+    ra_2d : numpy.ndarray
+        Array with 2D (image) representation of flux RA
+    dec_2d : numpy.ndarray
+        Array with 2D (image) representation of flux Dec
+    col_2d : numpy.ndarray
+        Array with 2D (image) representation of pixel column
+    row_2d : numpy.ndarray
+        Array with 2D (image) representation of pixel row
+    meta : dict
+        Dictionary with metadata
+    """
+    if not isinstance(fname, list):
+        fname = np.sort([fname])
+    flux, flux_err = [], []
+    times = []
+    telescopes = []
+    dct_types = []
+    campaigns = []
+    channels = []
+    for i, f in enumerate(fname):
+        if not os.path.isfile(f):
+            raise FileNotFoundError("FFI calibrated fits file does not exist: ", f)
+
+        hdul = fits.open(f)
+        header = hdul[0].header
+        telescopes.append(header["TELESCOP"])
+        campaigns.append(header["CAMPAIGN"])
+
+        # clusters only have one ext, bulge have multi-extensions
+        if len(hdul) > 1:
+            img_ext = 1
+        else:
+            img_ext = 0
+        channels.append(hdul[img_ext].header["CHANNEL"])
+        hdr = hdul[img_ext].header
+        times.append(Time([hdr["DATE-OBS"], hdr["DATE-END"]], format="isot").mjd.mean())
+        flux.append(hdul[img_ext].data)
+        if img_ext == 1:
+            flux_err.append(hdul[2].data)
+        else:
+            flux_err.append(np.sqrt(np.abs(hdul[img_ext].data)))
+
+        if i == 0:
+            wcs = WCS(hdr)
+
+    # check for integrity of files, same telescope, all FFIs and same quarter/campaign
+    if len(set(telescopes)) != 1:
+        raise ValueError("All FFIs must be from same telescope")
+
+    # collect meta data, I get everthing from one header.
+    attrs = [
+        "TELESCOP",
+        "INSTRUME",
+        "MISSION",
+        "DATSETNM",
+        "OBSMODE",
+        "OBJECT",
+    ]
+    meta = {k: header[k] for k in attrs if k in header.keys()}
+    attrs = [
+        "OBJECT",
+        "RADESYS",
+        "EQUINOX",
+        "BACKAPP",
+    ]
+    meta.update({k: hdr[k] for k in attrs if k in hdr.keys()})
+    meta.update({"EXTENSION": channels[0], "QUARTER": campaigns[0], "DCT_TYPE": "FFI"})
+    if "MISSION" not in meta.keys():
+        meta["MISSION"] = meta["TELESCOP"]
+
+    # sort by times
+    times = Time(times, format="btjd", scale="tdb")
+    tdx = np.argsort(times)
+    times = times[tdx]
+    row_2d, col_2d = np.mgrid[: flux[0].shape[0], : flux[0].shape[1]]
+    flux_2d = np.array(flux)[tdx]
+    flux_err_2d = np.array(flux_err)[tdx]
+
+    ra, dec = wcs.all_pix2world(np.vstack([col_2d.ravel(), row_2d.ravel()]).T, 0.0).T
+    ra_2d = ra.reshape(flux_2d.shape[1:])
+    dec_2d = dec.reshape(flux_2d.shape[1:])
+
+    del hdul, header, hdr, ra, dec
+
+    return (
+        wcs,
+        times,
+        flux_2d,
+        flux_err_2d,
+        ra_2d,
+        dec_2d,
+        col_2d,
+        row_2d,
+        meta,
+    )
